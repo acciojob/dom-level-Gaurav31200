7c012443b2faa8b555ea236bf97cf58d96ded4ab
Gaurav Saini <81917811+Gaurav31200@users.noreply.github.com>
2023-04-15 19:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function Alert(){
 	document.getElementById
-	alert("The level of the element is: " + 4 );
+	alert("The level of the element is: " + 5 );
 }
 Alert();
